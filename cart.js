a99ceecdf9803df71c53135658355cdd6f100bb6
Leplan Ludwig <leplan.ludwig@gmail.com>
2020-10-29 17:14:35 +0100

    Add files via upload

--- cart.js
+++ cart.js
@@ -1,8 +1,42 @@
 var cart = [];
+var cartHtml = [];
+
 function getCart() {
   cart = sessionStorage.getItem("cart");
   cart = JSON.parse(cart);
-  console.log(cart);
+  generateCartHtml(cart);
+}
+
+function generateCartHtml(cart) {
+  cartHtml = [];
+  cart.forEach((product, index) => {
+    cartHtml.push(
+      "<li class='list-group-item'>" +
+        product.title +
+        "<br>" +
+        product.description +
+        "<br>" +
+        product.price +
+        "<br>" +
+        '<button type="button" onClick="deleteProduct(\'' +
+        index +
+        "')\">" +
+        "Supprimer" +
+        "</button>" +
+        "</li>"
+    );
+  });
+  document.getElementById("cartSection").innerHTML = cartHtml.join("");
+}
+
+function returnToList() {
+  return (window.location.href = "./index.html");
+}
+
+function deleteProduct(index) {
+  delete cart[index];
+  const filter = cart.filter((elm) => elm);
+  generateCartHtml(filter);
 }
 
 getCart();
